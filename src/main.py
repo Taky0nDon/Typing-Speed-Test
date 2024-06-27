07d2515d394f2b1a8f45d53d8446f37d2a40523c
Taky0ndon <char@za.rd>
2024-06-26 22:14:34 -0400

    added entry widget where word is typed. mainly to test use of multiple frames. added quit button to terminate program

--- src/main.py
+++ src/main.py
@@ -5,29 +5,40 @@ from tkinter import ttk
 
 class Word:
     def __init__(self, frame: ttk.Frame, word: str) -> None:
-        self.label_content = word
-        self.word = tk.Label(frame, text=self.label_content)
+        self.word = word
+        self.word_label = tk.Label(frame, text=self.word)
 
 
 root = tk.Tk()
 root.title("Typing Speed Test")
 
-mainframe = ttk.Frame(root)
-wordframe = ttk.Frame(mainframe)
+wordframe = ttk.Frame(root)
+textbox_frame = ttk.Frame(root)
 
-mainframe.grid(column=0, row=0)
+wordframe.grid(column=0, row=0)
+textbox_frame.grid(column=0, row=1)
+
 with open("../assets/words.txt") as words:
     word_pool = words.read().strip().split("\n")
 col = 0
 row = 0
 for _ in range(50):
     current_word = choice(word_pool)
-    some_word = Word(mainframe, current_word)
+    some_word = Word(wordframe, current_word)
     if _ % 5 == 0 and _ > 0:
         row += 1
         col = 0
-    some_word.word.grid(column=col, row=row)
+    some_word.word_label.grid(column=col, row=row)
     col += 1
+
+typing_box_label = ttk.Label(textbox_frame, text="Start typing!")
+typing_box = ttk.Entry(textbox_frame)
+
+typing_box_label.grid(column=0, row=0)
+typing_box.grid(column=1, row=1)
+
+exit_button = ttk.Button(root, text="Quit", command=exit)
+exit_button.grid(column=0, row=2)
 
 root.mainloop()
 
