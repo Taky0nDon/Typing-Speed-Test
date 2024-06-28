9e78a136f386c6a299a88b5e1d9feb7f723d422b
Taky0ndon <char@za.rd>
2024-06-28 18:36:01 -0400

    created module for handling the Word objects, cleaning up main.py

---
+++ src/word.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from pathlib import Path
+from os import getcwd
+from tkinter import ttk
+
+
+WORD_FILE = Path(Path(getcwd()).parent/'assets/words.txt')
+class Word:
+    def __init__(self, frame: ttk.Frame, word: str) -> None:
+        self.word = word
+        self.word_label = tk.Label(frame, text=self.word)
+
+
+    @staticmethod
+    def generate_words() -> list[str]:
+        with open(WORD_FILE) as words:
+            word_pool = words.read().strip().split("\n")
+        return word_pool
+
+
+
+
