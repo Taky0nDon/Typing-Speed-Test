af73b1df9f1f1e1aa40912e108867b2dad649bf5
Taky0ndon <char@za.rd>
2024-07-14 22:43:39 -0400

    made the layout object global and constructed the score manager object globally. created a function that executes after a given time, which defines the length of the text. right now all it does is tell you what % of words you typed right.

--- src/main.py
+++ src/main.py
@@ -5,11 +5,24 @@ from word import Word, WordManager
 from gui import Layout
 from score_manager import ScoreManager
 
+TEST_LENGTH_MS = 10000
 
+
+score_manager = ScoreManager()
+gui = Layout(score_manager,
+             WordManager()
+             )
+
+def end_test():
+        words_typed = score_manager.missed + score_manager.correct
+        print(f"You typed {words_typed} "
+          f"words! You missed {score_manager.missed} of them. Your score " 
+          f"is {round(score_manager.correct / words_typed, 2) * 100}%!"
+          )
 
 def main() -> None:
-    gui = Layout(ScoreManager(),
-                 WordManager())
+    gui.root.after(TEST_LENGTH_MS, end_test)
+    gui.root.mainloop()
          
 
 
