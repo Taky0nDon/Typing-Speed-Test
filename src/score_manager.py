22012283e068513d17b19c84b8d5fbca382b40ca
Taky0ndon <mmaggio89@gmail.com>
2024-07-24 18:30:23 -0400

    Added function to calculate incorrect characters and not just words. Changed variable names to make difference between word error count and char error count.

--- src/score_manager.py
+++ src/score_manager.py
@@ -1,10 +1,42 @@
 class ScoreManager:
+    """
+    This class keeps track of how many words were typed correctly, and how
+    many contained errors. It has methods for incrementing each count.
+    """
     def __init__(self) -> None:
-       self.missed = 0
-       self.correct = 0
+       self.missed_words = 0
+       self.missed_chars = 0
+       self.correct_words = 0
+       self.correct_chars = 0
 
-    def increase_missed(self):
-        self.missed += 1
+    def increase_missed_count(self, target_word: str, typed_word: str):
+        self.missed_words += 1
+        self.calculate_char_errors(target=target_word, typed=typed_word)
 
-    def increase_correct(self):
-        self.correct += 1
+
+    def increase_correct_count(self, typed_word: str):
+        self.correct_words += 1
+        self.correct_chars = len(typed_word) + 1  # Need to add 1  to account
+                                                  # for space
+
+    def calculate_char_errors(self, target: str='', typed: str='') -> None:
+        """
+        Calculates number of  incorrect characters typed by counting number of
+        mismatched characters, and adding the difference in length of the strings.
+        """
+        if not target or not typed:
+            return
+        if len(typed) < len(target):
+            base, other = typed, target
+        else:
+            base, other = target, target
+
+        len_difference = len(other) - len(base)
+        idx = 0
+        for char in base:
+            if char != other[idx]:
+                self.missed_chars += 1
+            idx +=1 
+
+        self.missed_chars += len_difference
+
