2f125d393cfbeb5312510602989b96895a1f9ae2
Taky0ndon <mmaggio89@gmail.com>
2024-07-26 19:22:48 -0400

    replaced the end_test function with a method in the Layout class called terminate_grid that will wipe the grid layout and only display the final score and quit/restart buttons.

--- src/main.py
+++ src/main.py
@@ -1,33 +1,17 @@
-from tkinter import ttk
-from time import sleep
-
-from word import Word, WordManager
 from gui import Layout
 from score_manager import ScoreManager
-
-
-def end_test():
-    """This function is called when the test ends, either due to all words
-    being typed, or time running out.
-    """
-    words_typed = score_manager.missed_words + score_manager.correct_words
-    print(
-        f"You typed {words_typed} "
-        f"words! You missed {score_manager.missed_words} of them. Your score "
-        f"is {round(score_manager.correct_words / words_typed, 2) * 100}%!"
-    )
+from word import WordManager
 
 
 TEST_LENGTH_MS = 60000
 
 
-score_manager = ScoreManager()
-gui = Layout(score_manager, WordManager(), TEST_LENGTH_MS, end_test)
+gui = Layout(ScoreManager, WordManager, TEST_LENGTH_MS)
 
 
 def main() -> None:
     gui.root.after(1000, gui.decrement_countdown)
-    gui.root.after(TEST_LENGTH_MS, end_test)
+    gui.root.after(TEST_LENGTH_MS, gui.terminate_grid)
     gui.root.mainloop()
 
 
