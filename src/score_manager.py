d74a776b16ac2eac8dc08f9d14b9ec631a11e844
Taky0ndon <mmaggio89@gmail.com>
2024-09-04 18:30:03 -0400

    remove reference to correct and incorrect words in favor of focusing on characters. add function to calculate accuracy

--- src/score_manager.py
+++ src/score_manager.py
@@ -7,30 +7,9 @@ class ScoreManager():
     many contained errors. It has methods for incrementing each count.
     """
     def __init__(self) -> None:
-       self.missed_words = 0
-       self.missed_chars = 0
-       self.correct_words = 0
-       self.correct_chars = 0
-       self.typed_chars = 0
+       self.char_errors = 0
+       self.typed_entries = 0
        self.accuracy = "0.00%"
-
-    def update_accuracy(self) -> str:
-        """
-        Returns a float representing the number of correct characters divided
-        by total characters
-        """
-        if self.typed_chars > 0:
-            self.accuracy = f"{(self.correct_chars / self.typed_chars) * 100:.2f}%"
-            return self.accuracy
-        return self.accuracy
-
-    def increase_missed_count(self, target_word: str, typed_word: str):
-        self.missed_words += 1
-        self.calculate_char_errors(target=target_word, typed=typed_word)
-
-    def increase_correct_count(self, typed_word: str):
-        self.correct_words += 1
-        self.correct_chars += len(typed_word)
 
     def calculate_char_errors(self, target: str='', typed: str='') -> None:
         """
@@ -48,24 +27,24 @@ class ScoreManager():
         idx = 0
         for char in shorter:
             if char != longer[idx]:
-                self.missed_chars += 1
-            else:
-                self.correct_chars += 1
+                self.char_errors += 1
             idx +=1 
 
-        self.missed_chars += len_difference
+        self.char_errors += len_difference
 
-    def update_typed_chars(self, typed_content: str) -> int:
-        self.typed_chars = len(typed_content)
+    def update_typed_entries(self, typed_content: str) -> int:
+        self.typed_entries = len(typed_content)
         return len(typed_content)
 
-    def calculate_gross_wpm(self, length_of_time_ms: int) -> float:
-        print(f"{length_of_time_ms=}")
-        print(f"{self.typed_chars=}")
-        length_of_time_min = (length_of_time_ms) / (10**3 * 60)
-        wpm = ((self.typed_chars / 5) / length_of_time_min)
+    def calculate_gross_wpm(self, time_ms: int) -> float:
+        entries = self.typed_entries
+        print(f"{time_ms=}")
+        print(f"{self.typed_entries=}")
+        length_of_time_min = (time_ms) / (10**3 * 60)
+        words = entries / 5
+        wpm = round((words / length_of_time_min), 2)
         print(f"{length_of_time_min=}")
-        print(f"{wpm=}")
+        print(f" gross {wpm=:.2f}")
         return wpm
 
     def count_errors(self, target_words: list[str], typed_words: list[str]) -> int:
@@ -85,4 +64,15 @@ class ScoreManager():
                     if char != conjugate_char:
                         errors += 1
             print(f"{pair=},\n{sorted_by_len=}\n{errors=}")
+        self.char_errors = errors
         return errors
+
+    def update_accuracy(self) -> float:
+        print("HERE~!!!!!!!!!!")
+        print(f"{self.char_errors=}, {self.typed_entries=}")
+        correct_chars = self.typed_entries - self.char_errors
+        accuracy = correct_chars / self.typed_entries
+        accuracy_str = f"{accuracy: .2f}%"
+        self.accuracy = accuracy_str
+        return accuracy
+
